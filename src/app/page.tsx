1df076896129ac4afae08593a9a7d59d12e67585
ProjectAhkdan <ahkdanprika1@gmail.com>
2025-01-30 14:19:29 +0700

    wawawa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,35 @@
-'use client';
+"use client";
 
-import { Suspense } from 'react';
+import { Suspense } from "react";
 import Image from "next/image";
-import { motion } from "framer-motion";
 import NameParameter from "@/components/NameParameter";
 import { useSearchParams } from "next/navigation";
 import { IconMail, IconUser } from "@tabler/icons-react";
+import { motion } from "framer-motion";
+
+const leafAnimationTopRight = {
+  animate: {
+    y: [0, -10, 0],
+    rotate: [0, 2, -2, 0],
+    transition: {
+      duration: 4,
+      repeat: Infinity,
+      ease: "easeInOut",
+    },
+  },
+};
+
+const leafAnimationBottomLeft = {
+  animate: {
+    y: [0, 0, 0],
+    rotate: [-180, -170, -180],
+    transition: {
+      duration: 4,
+      repeat: Infinity,
+      ease: "easeInOut",
+    },
+  },
+};
 
 function HomeContent() {
   const searchParams = useSearchParams();
@@ -23,7 +47,7 @@ function HomeContent() {
 
       <div className="relative z-10 max-w-md flex flex-col h-screen">
         <div className="absolute top-[16%] w-full text-center z-30">
-          <h1 className="text-xl font-semibold text-[#2a374a]">
+          <h1 className="text-xl font-semibold text-[#2a374a] font-scheherazade">
             بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
           </h1>
         </div>
@@ -31,7 +55,9 @@ function HomeContent() {
           <h2 className=" text-7xl font-medium text-[#2a374a] font-greatvibes">
             Andrean
           </h2>
-          <h2 className=" text-5xl font-medium text-[#2a374a] font-greatvibes">&</h2>
+          <h2 className=" text-5xl font-medium text-[#2a374a] font-greatvibes">
+            &
+          </h2>
           <h2 className=" text-7xl font-meidum text-[#2a374a] font-greatvibes">
             Mutmainah
           </h2>
@@ -53,14 +79,23 @@ function HomeContent() {
           </div>
         </div>
 
-        <Image
+        <motion.img
           src="/elementkananatas.png"
           alt="rantingkayu"
           width={1080}
           height={540}
           className="absolute -top-[10%] -right-[50%] transform rotate-[30deg] "
+          {...leafAnimationTopRight}
         />
 
+        <motion.img
+          src="/elementkananatas.png"
+          alt="rantingkayu"
+          width={1080}
+          height={540}
+          className="absolute -bottom-[10%] -left-[50%] transform rotate-[-150deg] "
+          {...leafAnimationBottomLeft}
+        />
       </div>
     </div>
   );
