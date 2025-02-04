dba09a9cc05298adb8ea0ed8d5685105f28faf4c
Ahkdan Prika Nurwicaksono <ahkdanprika1@gmail.com>
2025-02-04 10:30:25 +0700

    Update countdown.tsx

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect, useMemo, useCallback, useRef } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 import { motion } from "framer-motion";
 import { weddingData } from "@/constans/data-dev";
 
@@ -12,13 +12,12 @@ type TimeRemaining = {
 
 const Countdown = () => {
   const targetDate = new Date(weddingData.countdown.eventDate).getTime();
-  const timeRemaining = useRef<TimeRemaining>({
+  const [timeRemaining, setTimeRemaining] = useState<TimeRemaining>({
     days: 0,
     hours: 0,
     minutes: 0,
     seconds: 0,
   });
-  const [, setForceRender] = useState(0);
 
   const [hasAnimated, setHasAnimated] = useState(false);
   const countdownRef = useRef<HTMLDivElement>(null);
@@ -26,39 +25,24 @@ const Countdown = () => {
   const updateTime = useCallback(() => {
     const now = new Date().getTime();
     const distance = targetDate - now;
+
     if (distance <= 0) {
-      timeRemaining.current = { days: 0, hours: 0, minutes: 0, seconds: 0 };
+      setTimeRemaining({ days: 0, hours: 0, minutes: 0, seconds: 0 });
     } else {
-      const newTime = {
+      setTimeRemaining({
         days: Math.floor(distance / (1000 * 60 * 60 * 24)),
-        hours: Math.floor(
-          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-        ),
+        hours: Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
         minutes: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
         seconds: Math.floor((distance % (1000 * 60)) / 1000),
-      };
-
-      if (newTime.minutes !== timeRemaining.current.minutes) {
-        timeRemaining.current = newTime;
-        setForceRender((prev) => prev + 1); 
-      }
+      });
     }
   }, [targetDate]);
 
-  const formattedTime = useMemo(() => timeRemaining, [timeRemaining]);
-
-  const countdownImage = useMemo(
-    () => (
-      <motion.img
-        src={weddingData.countdown.countdownImage}
-        alt="icon-left"
-        width={150}
-        height={150}
-        loading="lazy"
-      />
-    ),
-    []
-  );
+  useEffect(() => {
+    updateTime();
+    const interval = setInterval(updateTime, 1000);
+    return () => clearInterval(interval);
+  }, [updateTime]);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -67,31 +51,23 @@ const Countdown = () => {
           setHasAnimated(true);
         }
       },
-      { threshold: 0.5 }
+      { threshold: 0.2 } // Mengurangi threshold agar animasi lebih mudah dipicu
     );
-  
-    const currentRef = countdownRef.current; 
-  
+
+    const currentRef = countdownRef.current;
+
     if (currentRef) observer.observe(currentRef);
-  
+
     return () => {
-      if (currentRef) observer.unobserve(currentRef); 
+      if (currentRef) observer.unobserve(currentRef);
     };
   }, [hasAnimated]);
-  
-
-  useEffect(() => {
-    updateTime();
-    const interval = setInterval(updateTime, 1000);
-    return () => clearInterval(interval);
-  }, [updateTime]);
 
   return (
     <motion.div
       ref={countdownRef}
       className="relative flex text-white font-poppins items-center justify-center max-w-md m-auto overflow-hidden bg-[#435361] bg-noise pt-16 pb-16"
     >
-      {/* Gambar dengan animasi */}
       <motion.div
         className="absolute -left-10 top-[20%] transform -translate-y-1/2 rounded-full flex items-center justify-center -z-0"
         initial={{ rotate: 0 }}
@@ -102,13 +78,17 @@ const Countdown = () => {
           repeat: Infinity,
         }}
       >
-        {countdownImage}
+        <motion.img
+          src={weddingData.countdown.countdownImage}
+          alt="icon-left"
+          width={150}
+          height={150}
+          loading="lazy"
+        />
       </motion.div>
 
       <div className="flex flex-col items-center z-40">
-        <div className="text-5xl mb-8 font-greatvibes">
-          Catat Tanggalnya
-        </div>
+        <div className="text-5xl mb-8 font-greatvibes">Catat Tanggalnya</div>
 
         <div className="flex space-x-2 mb-6 z-40">
           {["days", "hours", "minutes", "seconds"].map((unit) => (
@@ -116,7 +96,7 @@ const Countdown = () => {
               key={unit}
               className="text-xl font-semibold text-center bg-white/15 backdrop-blur-sm rounded-xl p-2 font-montserrat"
             >
-              <div>{formattedTime.current[unit as keyof TimeRemaining]}</div>
+              <div>{timeRemaining[unit as keyof TimeRemaining]}</div>
               <div className="text-sm mt-1">
                 {unit === "days"
                   ? "Hari"
