1857b60244e94c9fe0896b4473ceac688568b0a4
Ahkdan Prika Nurwicaksono <ahkdanprika1@gmail.com>
2025-02-05 18:26:52 +0700

    Update SectionTwo.tsx

--- src/app/Andrean-Mutmainah/SectionTwo.tsx
+++ src/app/Andrean-Mutmainah/SectionTwo.tsx
@@ -20,7 +20,7 @@ const SectionTwo = ({ content }: Contents) => {
 
       <div className="realtive z-10 flex flex-col pt-20 pb-20">
           <h1 className="text-4xl font-bold font-greatvibes text-[#45535e]">
-            The Wedding Of
+            Walimatul 'ursy
           </h1>
         <div className="pt-10 relative flex justify-center overflow-hidden">
           <div className="bg-white/10 backdrop-blur-sm rounded-md shadow-sm ">
